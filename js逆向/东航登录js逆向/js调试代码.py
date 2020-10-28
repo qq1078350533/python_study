10ba33f8639d9f3af181c8c47d10d5d2cfe94eb0
snackdeng <2356125512@qq.com>
2020-10-28 20:06:04 +0800

    删除js逆向部分多余文件

--- js逆向/东航登录js逆向/js调试代码.py
+++
@@ -1,7 +0,0 @@
-import execjs
-import requests
-"64389594016"
-js = open('js调试.js', 'r', encoding='utf-8').read()
-ctx = execjs.compile(js, )
-result = ctx.call('pd', '111')
-print(result)
--- js逆向/财新登录密码js逆向/demo.py
+++
@@ -1,6 +0,0 @@
-import execjs
-import requests
-
-js = open('js调试.js', 'r', encoding='utf-8').read()
-ctx = execjs.compile(js)
-result = ctx.call('pwd')
