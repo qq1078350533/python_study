e4130c58796b2871b0b7ca2ddf44ace10378dee9
snackdeng <2356125512@qq.com>
2020-11-04 09:58:28 +0800

    同步更新20201104

--- Specialized_test/test2.py
+++ Specialized_test/test2.py
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> d0f0af6... 同步更新20201028
-sum = 0
-for i in range(1,10):
-    sum += i
 
-<<<<<<< HEAD
-print(sum)
-=======
-print(sum)
-=======
 # 极速版
 import requests,time
-import google.protobuf
+
 
 url = 'https://aweme.snssdk.com/aweme/v2/feed/?type=0&max_cursor=0&min_cursor=-1&count=6&volume=0.6666666666666666&pull_type=2&need_relieve_aweme=0&filter_warn=0&req_from&is_cold_start=0&longitude=0&latitude=0&address_book_access=1&gps_access=1&cached_item_num=3&last_ad_show_interval=-1&mac_address=f8%3Acf%3Ac5%3A88%3A7e%3A68&action_mask=-1&ad_extra=%7B%22topview_type%22%3A0%7D&pass_through&red_packet_guide=false&filter_live_cell=false&need_personal_recommend=1&is_first_feed=false&os_api=22&device_type=Nexus%206&ssmix=a&manifest_version_code=110700&dpi=560&uuid=359320051140506&app_name=douyin_lite&version_name=11.7.0&ts=1601097052&app_type=normal&ac=wifi&update_version_code=11709900&channel=tengxun&_rticket=1601097053242&device_platform=android&iid=3078249752832190&version_code=110700&mac_address=f8%3Acf%3Ac5%3A88%3A7e%3A68&cdid=c855715e-2555-4b5a-8374-8f1b107de99b&openudid=80c0065ee0be4cb&device_id=1952348995336375&resolution=1440*2392&os_version=5.1&language=zh&device_brand=google&aid=2329'
 headers = {
@@ -31,5 +19,4 @@ headers = {
 response = requests.get(url, headers=headers, verify=False)
 # response.encoding = 'utf-8'
 print(response.text)
->>>>>>> 60b95f6... 同步更新20201028
->>>>>>> d0f0af6... 同步更新20201028
+
--- Specialized_test/test6.py
+++ Specialized_test/test6.py
@@ -1,22 +1,45 @@
 import requests
-import re
-url = 'https://bam.nr-data.net/1/bd4e6041d0?a=190099556&v=1169.7b094c0&to=NQAAbUBQVhVZVhUIVgxKMVBGVFsJSlBBM1wTEAdKRh5rA1lHAgl/DgwFUUY%3D&rst=33808&ck=1&ref=https://www.jetstar.com/cn/zh/search-flights&ap=292&be=1406&fe=33183&dc=4599&af=err,xhr,stn,ins&perf=%7B%22timing%22:%7B%22of%22:1592900182595,%22n%22:0,%22f%22:22,%22dn%22:22,%22dne%22:22,%22c%22:24,%22s%22:278,%22ce%22:613,%22rq%22:614,%22rp%22:1352,%22rpe%22:1499,%22dl%22:1363,%22di%22:4227,%22ds%22:4599,%22de%22:4604,%22dc%22:33182,%22l%22:33183,%22le%22:33184%7D,%22navigation%22:%7B%7D%7D&fp=4119&fcp=4119&jsonp=NREUM.setToken'
-headers = {
-    "Host": "bam.nr-data.net",
-    "Connection": "keep-alive",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36",
-    "Accept": "*/*",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-Fetch-Mode": "no-cors",
-    "Sec-Fetch-Dest": "script",
-    "Referer": "https://www.jetstar.com/cn/zh/search-flights?origin=SIN&destination=HAK&flight-type=2&selected-departure-date=16-08-2020&selected-return-date=23-08-2020&adult=1&flexible=1&currency=SGD&sale-id=56978aa4-bc33-4bb0-90f8-9cd6258d9317",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-CN,zh;q=0.9",
-}
-response = requests.get(url,headers=headers,verify=False)
-a = response.headers['Set-Cookie'].split(';')[0].split('=')
-cookies = {
-    a[0] : a[1]
-}
-print(cookies)
-# JSESSIONID=b276ffa875ba097e
+import re, threading
+
+class Deng(threading.Thread):
+    def __init__(self):
+        self.session = requests.session()
+
+    def register(self, i):
+        url = "http://101.200.72.108/register"
+        data = {
+            'name': f'{i}',
+            'password': '111111',
+            'csrf_token': self.get(url),
+        }
+        res = self.session.post(url, data=data)
+        if res.text == '该用户名已经被注册':
+            print('注册失败')
+            with open('./file.txt', 'w', encoding='utf-8') as f:
+                f.write(f'{i}----111111')
+        else:
+            print('注册成功')
+            with open('./sucess.txt', 'w', encoding='utf-8') as f:
+                f.write(f'{i}----111111')
+
+    def get(self, url):
+        response = self.session.post(url)
+        token = re.findall('name="csrf_token".*?value="(.*?)">', response.text)[0]
+        return token
+
+    def login(self):
+        url = 'http://101.200.72.108/login'
+        data = {
+            'name': 'snackdeng',
+            'password': '111111',
+            'csrf_token': self.get(url),
+        }
+        res = self.session.post(self.url, data=data)
+        print(res.text)
+
+    def run(self):
+        for i in range(0,100000000000000):
+            self.register(i)
+
+if __name__ == '__main__':
+    Deng().run()
