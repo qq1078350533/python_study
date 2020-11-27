3cbc54da843a7901b9f142c0876e96027651eb37
snackdeng <2356125512@qq.com>
2020-11-27 10:22:03 +0800

    2020-11-27 新增11

--- Django项目/mysite/book/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-# 视图函数的第一个参数是HttpRequests类型的对象，里面包含了请求的所以参数
-# 视图函数的返回值是HttpRequests 类型的对象，里面包含了响应的数据
-def index(HttpRequest):
-    return HttpResponse("index")
