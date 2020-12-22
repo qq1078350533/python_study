5578bf92e65e8c98298da3707a4eff5ac90bc720
snackdeng <2356125512@qq.com>
2020-12-22 16:14:35 +0800

    更新微博评论的代码

--- python/爬虫/微博评论/demo.py
+++ python/爬虫/微博评论/demo.py
@@ -3,44 +3,41 @@ import jsonpath
 import time
 import pymongo
 
-client = pymongo.MongoClient('mongodb://admin:snackdeng@localhost:27017')
+client = pymongo.MongoClient('mongodb://admin:123456@192.168.86.128:27017')
 
 headers = {
+            "X-Sessionid": "1f916240-29b0-4d85-b7fa-056cad7f5e52",
+            "Authorization": "WB-SUT _2A95y5dbyDeRxGeNG41IW9CnEzDyIHXVvs206rDV6PUJbkdANLUb5kWpNSwhjTmhyfM7-l8kfbCvSIuDOS1fzpu-o",
             "accept-language": "zh-CN,zh;q=0.9",
             "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile Safari/537.36",
             "referer": "https://m.weibo.cn/detail/4485613145089303",
             "cookie": "WEIBOCN_FROM=1110005030; SUB=_2A25zeaLSDeRhGeNG41IW9CnEzDyIHXVQhc6arDV6PUJbkdAKLWnskW1NSwhjThESwIEYhgr4VhduwRl_bJaHXlPQ; SUHB=0mxkC0BeDArx0U; _T_WM=87866313336; MLOGIN=1; XSRF-TOKEN=882a81; M_WEIBOCN_PARAMS=oid%3D4485613145089303%26luicode%3D20000061%26lfid%3D4485613145089303"
         }
 
-max_id = ""
+max_id = "0"
+page = 1
 while True:
-    if max_id == "":
-        url = "https://m.weibo.cn/comments/hotflow?id=4485613145089303&mid=4485613145089303&max_id_type=0"
-    else:
-        url = "https://m.weibo.cn/comments/hotflow?id=4485613145089303&mid=4485613145089303&max_id={}&max_id_type=0".format(max_id)
+
+    url = f"https://api.weibo.cn/2/comments/build_comments?is_show_bulletin=2&c=android&from=10A9395010&count=200&max_id={max_id}&s=8f67a4f4&id=4584774314492510&gsid=_2A25y5dbyDeRxGeNG41IW9CnEzDyIHXVvs206rDV6PUJbkdANLUzGkWpNSwhjTgPJqpLlBg4wjaddP8pEX0T23WtU"
 
     response1 = requests.get(url=url,headers=headers)
     if response1.status_code != 200:
         break
     print("请求成功:{}".format(response1.status_code))
     response = response1.json()
-    names = jsonpath.jsonpath(response,"$..screen_name")
-    ids = jsonpath.jsonpath(response,"$..user.id")
-    max_id = jsonpath.jsonpath(response, "$..max_id")[0]
-    print("请求的是:{}".format(max_id))
-    texts = jsonpath.jsonpath(response,"$..text")
-    textss = []
-    for i in texts:
-        a = i.split("<",1)[0]
-        textss.append(a)
-    for id,name,text in zip(ids,names,textss):
+    for i in response['root_comments']:
+        name = i['user']['screen_name']
+        id = i['user']['id']
+        text = i['text']
         item1 = {
             "id" : id,
             "name" : name,
             "text" : text
         }
         client.weibo.pinglun.insert(item1)
-    print("写入成功")
+    print(f"第{page}页写入成功")
     client.close()
+    page += 1
+    max_id = response['max_id']
     time.sleep(3)
 
